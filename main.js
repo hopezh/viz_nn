9a9d11537bd0570b136d1886821527aff517110a
hopezh <hope.zh@gmail.com>
2021-09-30 21:26:29 +0800

    add nn_layer class

---
+++ main.js
@@ -0,0 +1,8 @@
+// [+] import modules
+import * as THREE from "three";
+import NNLayer from "./src/nn_layer.js";
+
+// const layer1 = new NNLayer();
+// layer1.name = "input";
+// layer1.size = 100;
+// console.log(layer1);
---
+++ src/nn_layer.js
@@ -0,0 +1,9 @@
+import * as THREE from "three";
+
+export default class NNLayer {
+    constructor(name, size) {
+        //options
+        this.name = name;
+        this.size = size;
+    }
+}
