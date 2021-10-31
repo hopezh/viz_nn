cc7e6455fcd6fe03628fedc2d0bf48e8e849a2bc
hopezh <hope.zh@gmail.com>
2021-10-31 19:35:14 +0800

    make main.js a min template

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+// [#] import modules
 import * as THREE from "three";
-
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { Vector3 } from "three";
+import Stats from "three/examples/jsm/libs/stats.module.js";
+import { GUI } from "three/examples/jsm/libs/dat.gui.module.js";
 
 import * as tf from "@tensorflow/tfjs";
 // import { tensor } from "@tensorflow/tfjs-core";
@@ -9,15 +12,121 @@ import Util from "./src/Util.js";
 
 import Stack from "./src/Stack.js";
 
-// ----------------------------------------------------------------
+// ============================================================
+// [#] init vars
+let scene;
+let cameraPerspective;
+let rendererWebGL;
+let controlsWebGL;
+let stats;
+let gui;
 
+// ============================================================
+// [#] run main functions
+init();
+animate();
+
+// ============================================================
+// [#] init
+function init() {
+    // [+] scene
+    scene = new THREE.Scene();
+
+    // [+] camera
+    // [-] perspective camera
+    cameraPerspective = new THREE.PerspectiveCamera(
+        50,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        100000
+    );
+    cameraPerspective.position.set(10, 20, 30);
+
+    // [+] light
+    // [-] direction light
+    const dirLight = new THREE.DirectionalLight(0xffffff, 1);
+    dirLight.position.set(1, 1, 1).normalize();
+    scene.add(dirLight);
+
+    // [-] ambient light
+    const ambLight = new THREE.AmbientLight(0x555555, 0.5);
+    scene.add(ambLight);
+
+    // [+] helper
+    // [-] grid helper
+    const grid_size = 20;
+    const grid_div = 20;
+    const grid_colorCenLine = "rgb(100, 100, 100)";
+    const grid_colorGrid = "rgb(50, 50, 50)";
+    const gridHelper = new THREE.GridHelper(
+        grid_size,
+        grid_div,
+        grid_colorCenLine,
+        grid_colorGrid
+    );
+    gridHelper.position.set(0, 0, 0);
+    scene.add(gridHelper);
+
+    // [-] axis helper
+    const axesHelper = new THREE.AxesHelper(100);
+    axesHelper.position.set(-1000, -0, -1000);
+    scene.add(axesHelper);
+
+    // [+] renderers
+    // [-] webgl renderer
+    rendererWebGL = new THREE.WebGLRenderer({ antialias: true });
+    rendererWebGL.setPixelRatio(window.devicePixelRatio);
+    rendererWebGL.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(rendererWebGL.domElement);
+    rendererWebGL.shadowMap.enabled = true;
+    rendererWebGL.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
+
+    // [+] controls
+    const controlsCSS = new OrbitControls(
+        cameraPerspective,
+        rendererWebGL.domElement
+    );
+
+    // [+] stats
+    stats = new Stats();
+    document.body.appendChild(stats.dom);
+
+    // [+] window event listener
+    window.addEventListener("resize", onWindowResize, false);
+}
+
+// ============================================================
+// [#] onWindowResize
+function onWindowResize() {
+    cameraPerspective.aspect = window.innerWidth / window.innerHeight;
+    cameraPerspective.updateProjectionMatrix();
+    rendererWebGL.setSize(window.innerWidth, window.innerHeight);
+}
+
+// ============================================================
+// [#] animate
+function animate() {
+    requestAnimationFrame(animate);
+    render();
+    stats.update();
+}
+
+// ============================================================
+// [#] render
+function render() {
+    // rendererCSS3D.render(scene, camera);
+    rendererWebGL.render(scene, cameraPerspective);
+}
+
+// ============================================================
+// [T] Test
 // create a random tensor
 const tensor = tf.randomNormal([3, 4, 5]);
-tensor.print();
+// tensor.print();
 
 // console.log(tensor.constructor.name == "Tensor");
 
-const s2 = new Stack(42, tensor);
+// const s2 = new Stack(42, tensor);
 
 // const s2_tensor = s2.tensor;
 // console.log("s2, tensor : ", s2_tensor);
